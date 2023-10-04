9582578f0593d702148a0260cb892ed28c5cb56d
Nikita Egorov <khenarghot@protonmail.com>
2023-10-04 20:02:54 +0300

    Fix test for EXTINF check
    The empty segment uri is forbidden by RFC. I add something reasonable.

--- reader_test.go
+++ reader_test.go
@@ -597,6 +597,7 @@ func TestDecodeMediaPlaylistExtInfNonStrict2(t *testing.T) {
 #EXT-X-VERSION:3
 #EXT-X-MEDIA-SEQUENCE:0
 %s
+path
 `
 
 	tests := []struct {
@@ -636,6 +637,7 @@ func TestDecodeMediaPlaylistExtInfNonStrict2(t *testing.T) {
 		if err != nil {
 			t.Errorf("unexpected error: %v", err)
 		}
+		test.wantSegment.URI = "path"
 		if !reflect.DeepEqual(p.Segments[0], test.wantSegment) {
 			t.Errorf("\nhave: %+v\nwant: %+v", p.Segments[0], test.wantSegment)
 		}
